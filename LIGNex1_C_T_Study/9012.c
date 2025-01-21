dd876fb0439369994390a9c95d2ccf4e5a8b9b59
JhiWhoon Kang <109329462+JhiWhoonKang@users.noreply.github.com>
2025-01-21 17:44:26 +0900

    20250121
    20250121

---
+++ LIGNex1_C_T_Study/9012.c
@@ -0,0 +1,19 @@
+#define _CRT_NO_SECURE_WARNINGS_
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	int T;
+	(void)scanf("%d", &T);
+	char arr[T][51];
+
+	for (int i = 0;i < T;++i)
+	{
+		(void)scanf(" %s", )
+	}
+
+
+	return 0;
+}
