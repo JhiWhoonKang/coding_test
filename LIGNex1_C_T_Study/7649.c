ee2b3abf111369986d0e2d189911110a151a6f66
JhiWhoon Kang <kangjw0914@naver.com>
2025-02-06 00:52:24 +0900

    20250206
    20250206

--- LIGNex1_C_T_Study/7649.c
+++ LIGNex1_C_T_Study/7649.c
@@ -15,7 +15,7 @@ typedef struct _Edge
 
 Edge* tree[MAX + 1];
 int N, M;			// N: 정점의 개수, M: 문자열의 길이
-char S[MAX];	// input: 입력 문자열, 좋아하는 산책길 종류 순서
+char S[MAX + 1];	// input: 입력 문자열, 좋아하는 산책길 종류 순서
 
 void AddEdge(int u, int v, char c)
 {
@@ -29,6 +29,7 @@ void AddEdge(int u, int v, char c)
 // 시작점: 항상 1번 정점
 // 끝점: 1번 정점을 루트로 봤을 때의 리프 노드 중 하나
 int maxHappy = 0;
+char path[MAX + 1];
 void DFS(int node, int parent, char* path, int path_len) 
 {
     Edge* edge = *(tree + node);
@@ -39,7 +40,8 @@ void DFS(int node, int parent, char* path, int path_len)
         if (edge->to != parent) // 부모 노드로 되돌아가지 않음
         {
             is_leaf = 0;
-            path[path_len] = edge->c;            
+            path[path_len] = edge->c;
+            path[path_len + 1] = '\0';
             DFS(edge->to, node, path, ++path_len);
         }
         edge = edge->next;
@@ -47,7 +49,6 @@ void DFS(int node, int parent, char* path, int path_len)
 
     if (is_leaf) // 리프까지 왔으니까 문자열 비교
     {
-        path[path_len + 1] = '\0';
         int happy = LCS(path, S, path_len, M);
         if (happy > maxHappy)
         {
@@ -59,9 +60,9 @@ void DFS(int node, int parent, char* path, int path_len)
 int maxSearch[MAX + 1][MAX + 1];        // dp 돌릴거
 int LCS(char*ss, char *SS, int s_len, int S_len)
 {   
-    for (int i = 1;i <= s_len;++i)
+    for (int i = 1; i <= s_len; ++i)
     {
-        for (int j = 1;j <= S_len;++j)
+        for (int j = 1; j <= S_len; ++j)
         {
             if (*(ss + (i - 1)) == *(SS + (j - 1))) // for문 돌리다가 문자 같은거 발견
             {
@@ -69,26 +70,11 @@ int LCS(char*ss, char *SS, int s_len, int S_len)
             }
             else // 다르면 dp 원리로 
             {
-                maxSearch[i][j] = ;
+                maxSearch[i][j] = (maxSearch[i - 1][j] > maxSearch[i][j - 1]) ? maxSearch[i - 1][j] : maxSearch[i][j - 1];
             }
         }
     }
-    return;
-}
-
-int compute_LCS(char* str1, int len1, char* str2, int len2) {
-    int dp[MAX_M + 1][MAX_M + 1] = { 0 };
-    for (int i = 1; i <= len1; i++) {
-        for (int j = 1; j <= len2; j++) {
-            if (str1[i - 1] == str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            }
-            else {
-                dp[i][j] = (dp[i - 1][j] > dp[i][j - 1]) ? dp[i - 1][j] : dp[i][j - 1];
-            }
-        }
-    }
-    return dp[len1][len2];
+    return maxSearch[s_len][S_len];
 }
 
 int main(void)
@@ -103,101 +89,11 @@ int main(void)
         AddEdge(u, v, c);
         AddEdge(v, u, c);
     }
+    
+    DFS(1, -1, path, 0);
 
-
+    printf("%d\n", maxHappy);
 
 	return 0;
 }
-#endif
-
-#if 0
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define MAX_N 5000
-#define MAX_M 5000
-
-// 트리의 간선을 저장할 구조체
-typedef struct Edge {
-    int to;
-    char c;
-    struct Edge* next;
-} Edge;
-
-// 트리 저장을 위한 배열
-Edge* tree[MAX_N + 1];
-int N, M;
-char S[MAX_M + 1];
-int max_happiness = 0;
-
-// 간선 추가 함수
-void add_edge(int u, int v, char c) {
-    Edge* edge = (Edge*)malloc(sizeof(Edge));
-    edge->to = v;
-    edge->c = c;
-    edge->next = tree[u];
-    tree[u] = edge;
-}
-
-// LCS 계산 함수 (길이가 len1인 str1과 len2인 str2)
-int compute_LCS(char* str1, int len1, char* str2, int len2) {
-    int dp[MAX_M + 1][MAX_M + 1] = { 0 };
-    for (int i = 1; i <= len1; i++) {
-        for (int j = 1; j <= len2; j++) {
-            if (str1[i - 1] == str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            }
-            else {
-                dp[i][j] = (dp[i - 1][j] > dp[i][j - 1]) ? dp[i - 1][j] : dp[i][j - 1];
-            }
-        }
-    }
-    return dp[len1][len2];
-}
-
-// DFS 탐색 함수
-void dfs(int node, int parent, char* path, int path_len) {
-    Edge* edge = tree[node];
-    int is_leaf = 1;
-
-    while (edge) {
-        if (edge->to != parent) { // 부모 노드로 되돌아가지 않음
-            is_leaf = 0;
-            path[path_len] = edge->c;
-            path[path_len + 1] = '\0';
-            dfs(edge->to, node, path, path_len + 1);
-        }
-        edge = edge->next;
-    }
-
-    if (is_leaf) {
-        int happiness = compute_LCS(path, path_len, S, M);
-        if (happiness > max_happiness) {
-            max_happiness = happiness;
-        }
-    }
-}
-
-int main() {
-    scanf("%d %d", &N, &M);
-    scanf("%s", S);
-
-    for (int i = 0; i < N - 1; i++) {
-        int u, v;
-        char c;
-        scanf("%d %d %c", &u, &v, &c);
-        add_edge(u, v, c);
-        add_edge(v, u, c);
-    }
-
-    char path[MAX_N + 1];
-    dfs(1, -1, path, 0);
-
-    printf("%d\n", max_happiness);
-    return 0;
-}
-
 #endif
