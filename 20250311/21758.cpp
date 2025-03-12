908527078df1c2d953d2423b0e51e1e870e31802
JhiWhoon Kang <109329462+JhiWhoonKang@users.noreply.github.com>
2025-03-12 20:26:41 +0900

    jhi
    jhi

---
+++ 20250311/21758.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	// 연한 회색: 벌이 있는 장소
+	// 진한 회색: 벌통이 있는 장소
+	// 벌통으로 날아가면서 지나가는 모든 칸에 꿀 수집
+	// 칸에 있는 숫자는 꿀을 수집할 수 있는 양(벌통 포함)
+	// 시작 장소는 불가
+
+
+	return 0;
+}
